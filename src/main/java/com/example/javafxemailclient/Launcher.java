fd0504abe98f8a3089710739dcae697d92e77946
danielSzkotak <daniel.szkotak.programista@gmail.com>
2022-10-24 21:13:30 +0200

    Added dependecies for my MainWindow - WebView - working

--- src/main/java/com/example/javafxemailclient/Launcher.java
+++ src/main/java/com/example/javafxemailclient/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.web.WebView;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -22,10 +23,10 @@ public class Launcher extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent parent = FXMLLoader.load(getClass().getResource("view/LoginWindow.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("view/MainWindow.fxml"));
         //FXMLLoader parent = new FXMLLoader(Launcher.class.getResource("firstFXML.fxml"));
 
-        Scene scene = new Scene(parent, 495, 370);
+        Scene scene = new Scene(parent);
         primaryStage.setScene(scene);
         primaryStage.show();
 
