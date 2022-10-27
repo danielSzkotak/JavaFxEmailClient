799a360cb8a9957353b585f0c1373f7be24259cc
danielSzkotak <daniel.szkotak.programista@gmail.com>
2022-10-27 18:55:33 +0200

    Added better css for different thems

--- src/main/java/com/example/javafxemailclient/Launcher.java
+++ src/main/java/com/example/javafxemailclient/Launcher.java
@@ -25,7 +25,8 @@ public class Launcher extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         ViewFactory viewFactory = new ViewFactory(new EmailManager());
-        viewFactory.showLoginWindow();
+        viewFactory.showOptionsWindow();
+        viewFactory.updateStyles();
 
     }
 }
