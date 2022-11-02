8fd4cbccdb8971836ff8364060cd7f7e0f42cb1e
danielSzkotak <daniel.szkotak.programista@gmail.com>
2022-11-02 21:25:18 +0100

    Added JavaMail setup, EmailAccount setup, Login Service

--- src/main/java/com/example/javafxemailclient/controller/LoginWindowController.java
+++ src/main/java/com/example/javafxemailclient/controller/LoginWindowController.java
@@ -1,6 +1,8 @@
 package com.example.javafxemailclient.controller;
 
 import com.example.javafxemailclient.EmailManager;
+import com.example.javafxemailclient.controller.services.LoginService;
+import com.example.javafxemailclient.model.EmailAccount;
 import com.example.javafxemailclient.view.ViewFactory;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -16,7 +18,7 @@ public class LoginWindowController extends BaseController {
     private Label errorLabel;
 
     @FXML
-    private PasswordField passwordFiled;
+    private PasswordField passwordField;
 
     public LoginWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
         super(emailManager, viewFactory, fxmlName);
@@ -25,11 +27,34 @@ public class LoginWindowController extends BaseController {
     @FXML
     void loginButtonAction() {
 
+        if (fieldsAreValid()){
+            EmailAccount emailAccount = new EmailAccount(emailField.getText(), passwordField.getText());
+            LoginService loginService = new LoginService(emailAccount, emailManager);
+            EmailLoginResult emailLoginResult = loginService.login();
+
+            switch (emailLoginResult){
+                case SUCCESS:
+                    System.out.println("login succesfull !!!" + emailAccount);
+                    return;
+            }
+        }
         System.out.println("loginButtonAction!");
         viewFactory.showMainWindow();
         Stage stage = (Stage) errorLabel.getScene().getWindow();
         viewFactory.closeStage(stage);
 
+    }
+
+    private boolean fieldsAreValid() {
+        if (emailField.getText().isEmpty()){
+            errorLabel.setText("Please fill email");
+            return false;
+        }
+        if (passwordField.getText().isEmpty()){
+            errorLabel.setText("Please fill password");
+            return false;
+        }
+        return true;
     }
 
 }
