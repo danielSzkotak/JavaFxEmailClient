ab5a1471070387dbd6e188d30f2ab3ad923f3cf9
danielSzkotak <daniel.szkotak.programista@gmail.com>
2022-11-15 19:40:40 +0100

    Added new income email show in email folders and main window

--- src/main/java/com/example/javafxemailclient/EmailManager.java
+++ src/main/java/com/example/javafxemailclient/EmailManager.java
@@ -1,11 +1,18 @@
 package com.example.javafxemailclient;
 
 import com.example.javafxemailclient.controller.services.FetchFolderService;
+import com.example.javafxemailclient.controller.services.FolderUpdaterService;
 import com.example.javafxemailclient.model.EmailAccount;
 import com.example.javafxemailclient.model.EmailTreeItem;
 import javafx.scene.control.TreeItem;
 
+import javax.mail.Folder;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmailManager {
+
+    private FolderUpdaterService folderUpdaterService;
 
     //Folder handling:
     private EmailTreeItem<String> foldersRoot = new EmailTreeItem<>("");
@@ -14,9 +21,19 @@ public class EmailManager {
         return foldersRoot;
     }
 
+    private List<Folder> folderList = new ArrayList<Folder>();
+    public List<Folder> getFolderList(){
+        return this.folderList;
+    }
+
+    public EmailManager(){
+        folderUpdaterService = new FolderUpdaterService(folderList);
+        folderUpdaterService.start();
+    }
+
     public void addEmailAccount(EmailAccount emailAccount){
         EmailTreeItem<String> treeItem = new EmailTreeItem<>(emailAccount.getAddress());
-        FetchFolderService fetchFolderService = new FetchFolderService(emailAccount.getStore(), treeItem);
+        FetchFolderService fetchFolderService = new FetchFolderService(emailAccount.getStore(), treeItem, folderList);
         fetchFolderService.start();
         foldersRoot.getChildren().add(treeItem);
     }
--- src/main/java/com/example/javafxemailclient/controller/services/FetchFolderService.java
+++ src/main/java/com/example/javafxemailclient/controller/services/FetchFolderService.java
@@ -5,17 +5,23 @@ import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 
 import javax.mail.Folder;
+import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Store;
+import javax.mail.event.MessageCountEvent;
+import javax.mail.event.MessageCountListener;
+import java.util.List;
 
 public class FetchFolderService extends Service<Void> {
 
     private Store store;
     private EmailTreeItem<String> folderRoot;
+    private List<Folder> folderList;
 
-    public FetchFolderService(Store store, EmailTreeItem<String> folderRoot) {
+    public FetchFolderService(Store store, EmailTreeItem<String> folderRoot, List<Folder> folderList) {
         this.store = store;
         this.folderRoot = folderRoot;
+        this.folderList = folderList;
     }
 
     @Override
@@ -36,16 +42,38 @@ public class FetchFolderService extends Service<Void> {
 
     private void handleFoldedrs(Folder[] folders, EmailTreeItem<String> folderRoot) throws MessagingException {
         for (Folder folder: folders){
+            folderList.add(folder);
             EmailTreeItem<String> emailTreeItem = new EmailTreeItem<String>(folder.getName());
             folderRoot.getChildren().add(emailTreeItem);
             folderRoot.setExpanded(true);
-
             fetchMessagesOnFolder(folder, emailTreeItem);
+            addMessageListenerToFolder(folder, emailTreeItem);
             if (folder.getType() == Folder.HOLDS_FOLDERS){
                 Folder[] subFolders = folder.list();
                 handleFoldedrs(subFolders, emailTreeItem);
             }
         }
+    }
+
+    private void addMessageListenerToFolder(Folder folder, EmailTreeItem<String> emailTreeItem) {
+        folder.addMessageCountListener(new MessageCountListener() {
+            @Override
+            public void messagesAdded(MessageCountEvent messageCountEvent) {
+                for (int i = 0; i < messageCountEvent.getMessages().length; i++){
+                    try {
+                        Message message = folder.getMessage(folder.getMessageCount() - i);
+                        emailTreeItem.addEmailToTop(message);
+                    } catch (MessagingException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+
+            @Override
+            public void messagesRemoved(MessageCountEvent messageCountEvent) {
+                System.out.println("message remove event" + messageCountEvent);
+            }
+        });
     }
 
     private void fetchMessagesOnFolder(Folder folder, EmailTreeItem<String> emailTreeItem) {
